7f360d36258c72460afe9fa1bc762cf6f806a0af
Uriel <uriel.lira@smartsuite.imd.ufrn.br>
2024-11-27 10:14:38 -0300

    create env variable with app name

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,18 @@
+import { Typography } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 
 export const Header = () => {
   return (
     <AppBar>
-      <Toolbar>CryptoDashboard</Toolbar>
+      <Toolbar>
+        <Typography
+          component={"h1"}
+          sx={{ textTransform: "uppercase", fontWeight: "bold" }}
+        >
+          {import.meta.env.VITE_APP_NAME}
+        </Typography>
+      </Toolbar>
     </AppBar>
   );
 };
